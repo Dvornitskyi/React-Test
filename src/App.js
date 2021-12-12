926129d4a7c90f0b097512b60e765034b26af54a
Yan Dvornitskyi <dvornitskyi@gmail.com>
2021-12-12 18:57:07 +0200

    add form and small todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Test from './component/Test';
+import Form from './component/Form'
 
 
 function App() {
   return (
     <div className="App">
+      <Form />
       <Test/>
     </div>
   );
